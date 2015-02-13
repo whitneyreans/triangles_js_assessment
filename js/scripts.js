f8a0a0a5111ed84645cda448a3757f647d3d0cba
Whitney <wreans@yahoo.com>
2015-02-13 09:45:16 -0800

    Add initial files and equilateral spec

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var triangle = function(side1, side2, side3) {
+
+  if (side1 === side2 && side2 === side3) {
+    return true;
+  }
+}
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe('triangle', function() {
+  it("returns true if the numbers entered make an equilateral triangle", function() {
+    expect(triangle(4, 4, 4)).to.eql(true);
+  });
+});
