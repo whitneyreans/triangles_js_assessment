72a9d64b4dddd0a64218e32f40809e5537496cc1
Whitney <wreans@yahoo.com>
2015-02-13 10:46:12 -0800

    add html file and jquery

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,32 @@ var triangle = function(side1, side2, side3) {
     return "not a triangle";
   } else if (side1 === side2 && side2 === side3) {
     return "equilateral";
-  } else if (side1 === side2  || side2 === side3 || side1 === side3) {
+  } else if (side1 === side2 || side2 === side3 || side1 === side3) {
     return "isosceles";
+  } else {
+    return "scalene";
   }
 }
+
+
+$(document).ready(function() {
+  $("form#triangles").submit(function(event) {
+    var side1 = $("input#side1").val();
+    var side2 = $("input#side2").val();
+    var side3 = $("input#side3").val();
+    var result = triangle(side1, side2, side3);
+
+    if (result === "not a triangle") {
+      $(".sentence").text("This is not a triangle!");
+    } else if (result === "equilateral") {
+      $(".sentence").text("This is an equilateral triangle!");
+    } else if (result === "isosceles") {
+      $(".sentence").text("This is an isosceles triangle!");
+    } else if (result === "scalene") {
+      $(".sentence").text("This is a scalene triangle!");
+    }
+
+    $("#results").show();
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,4 +1,9 @@
 describe('triangle', function() {
+
+  it("returns not a triangle if the numbers entered do not make a triangle", function() {
+    expect(triangle(2, 2, 8)).to.equal("not a triangle");
+  });
+
   it("returns equilateral if the numbers entered make an equilateral triangle", function() {
     expect(triangle(4, 4, 4)).to.equal("equilateral");
   });
@@ -7,7 +12,8 @@ describe('triangle', function() {
     expect(triangle(4, 4, 5)).to.equal("isosceles");
   });
 
-  it("returns not a triangle if the numbers entered do not make a triangle", function() {
-    expect(triangle(2, 2, 8)).to.equal("not a triangle");
+  it("returns scalene if no sides are equal", function() {
+    expect(triangle(6, 5, 4)).to.equal("scalene");
   });
+
 });
