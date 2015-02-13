6f3ab2f0ce2bc3f50e906ccb719342939405379e
Whitney <wreans@yahoo.com>
2015-02-13 09:58:25 -0800

    add isosceles and non triangle specsand code, while changing order of operations

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 var triangle = function(side1, side2, side3) {
 
-  if (side1 === side2 && side2 === side3) {
-    return true;
+  if (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2) {
+    return "not a triangle";
+  } else if (side1 === side2 && side2 === side3) {
+    return "equilateral";
+  } else if (side1 === side2  || side2 === side3 || side1 === side3) {
+    return "isosceles";
   }
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,13 @@
 describe('triangle', function() {
-  it("returns true if the numbers entered make an equilateral triangle", function() {
-    expect(triangle(4, 4, 4)).to.eql(true);
+  it("returns equilateral if the numbers entered make an equilateral triangle", function() {
+    expect(triangle(4, 4, 4)).to.equal("equilateral");
+  });
+
+  it("returns isosceles if the numbers entered make an isosceles triangle", function() {
+    expect(triangle(4, 4, 5)).to.equal("isosceles");
+  });
+
+  it("returns not a triangle if the numbers entered do not make a triangle", function() {
+    expect(triangle(2, 2, 8)).to.equal("not a triangle");
   });
 });
